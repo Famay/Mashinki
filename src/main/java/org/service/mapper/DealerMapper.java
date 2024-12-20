a924dc1a3b2dae993836c7a655fa171ddf5d41c2
Famay <voroninandrey2005@gmail.com>
2024-12-21 02:48:19 +0800

    ...

---
+++ src/main/java/org/service/mapper/DealerMapper.java
@@ -0,0 +1,15 @@
+package org.service.mapper;
+
+import org.entity.DealerEntity;
+import org.dto.DealerCenterDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface DealerMapper {
+    DealerMapper INSTANCE = Mappers.getMapper(DealerMapper.class);
+
+    DealerCenterDTO dealerEntityToDealerCenterDTO(DealerEntity dealerEntity);
+    DealerEntity dealerCenterDTOToDealerEntity(DealerCenterDTO dealerCenterDTO);
+}
